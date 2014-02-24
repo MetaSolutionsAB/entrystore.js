34f677b29081fe3294426d6e864e8823cb08eae2
matthiaspalmer <mpalmer@gmail.com>
2014-02-24 22:20:44 +0100

    buildscript that works with both node and nodejs

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-../lib/dojo-src/util/buildscripts/build.sh --help --bin node --profile storejs.profile.js
+cd ../lib/dojo-src
+NODE_PATH=$(which node)
+if [ -x "$NODE_PATH" ] ; then
+    node dojo/dojo.js load=build --profile ../../build/storejs.profile.js --release 
+else
+    nodejs dojo/dojo.js load=build --profile ../../build/storejs.profile.js --release 
+fi
+
