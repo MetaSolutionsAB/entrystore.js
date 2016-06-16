a2f1da4e9e1316fe64205defaccc2f988b49354f
Matthias Palmér <matthias@metasolutions.se>
2016-06-16 11:04:45 +0200

    Smarter support for setting transform property

--- Pipeline.js
+++ Pipeline.js
@@ -280,12 +280,14 @@ define([
     };
 
     /**
-     * Sets or updates an individual property (key-value pair in arguments) of a transform
+     * Sets or updates an individual property (key-value pair in arguments) of a transform.
+     * The transform is identified either explicitly by an id or via a transformType (assumed unique).
      * @param {string} transformId the transform to change the property for
      * @param {string} key
      * @param {string} value
      */
     Pipeline.prototype.setTransformProperty = function(transformId, key, value) {
+        if (this._graph.isBlank)
         var obj = this.getTransformArguments(transformId);
         if (obj != null) {
             obj[key] = value;
