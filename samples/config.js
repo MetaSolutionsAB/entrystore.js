8b411147ceb0505f8ad3dfcc1ec44322476514a1
Matthias Palmér <matthias@metasolutions.se>
2016-06-30 12:17:34 +0200

    Dependencies for samples updated

--- samples/config.js
+++ samples/config.js
@@ -1,7 +1,9 @@
 var dojoConfig = {
     async:true,
     packages: [
-        { name: "store", location: "../.." }]
+        { name: "store", location: "../.."},
+        { name: "md5", location: "../../libs/md5/js", main: "md5"}
+    ]
 };
 var test_username = "Donald";
 var test_password = "donalddonald";
