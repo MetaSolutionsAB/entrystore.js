d1d7d32073ee35ac00dd9ce8cf6872c6ca8dd9a8
Matthias Palmér <matthias@metasolutions.se>
2016-09-13 07:33:46 +0200

    Dependency to Blob in new Dojo must be patched to work in nodejs

--- config/fix.js
+++ config/fix.js
@@ -1,5 +1,8 @@
 define([
     'dojo/_base/kernel'
 ], function(kernel) {
+    //Polyfill for Blob in node environment, expected to exist by dojo/request/util line 124
+    Blob = function() {};
+    //Dojo initialization hack when using require.js loader
     kernel.global.require = this.requirejsVars;
 });
