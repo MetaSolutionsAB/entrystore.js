8710b2ed93ede4ab64a4632cf885cb966c90d645
Valentino Hudhra <v.hudhra@gmail.com>
2019-08-12 16:13:13 +0200

    Update tests File.js

--- tests/File.js
+++ tests/File.js
@@ -2,82 +2,101 @@ const _ = require('lodash');
 const { EntryStore, utils } = require('../dist/EntryStore.node');
 const config = require('./config');
 
-const es = new EntryStore(config.repository);
-const context = es.getContextById('1');
-let ready;
+const { repository, adminUser, adminPassword } = config;
+let context;
+let finished = false;
+
+const setUp = async (callback) => {
+  if (!context) {
+    const es = new EntryStore(repository);
+    const auth = es.getAuth();
+    await auth.logout();
+    await auth.login(adminUser, adminPassword, 698700);
+    const contextEntry = await es.newContext().commit();
+    context = contextEntry.getResource(true);
+  }
+  callback();
+};
+
+const tearDown = async (callback) => {
+  if (finished) {
+    try {
+      const contextEntry = await context.getEntry();
+      await contextEntry.del(true);
+
+      const es = new EntryStore(repository);
+      const auth = es.getAuth();
+      await auth.logout();
+    } catch (err) {
+      // console.error(err);
+    }
+  }
+  callback();
+};
 
 exports.File = {
-  setUp(callback) {
-    if (!ready) {
-      es.auth({ user: 'Donald', password: 'donalddonald' }).then(() => {
-        ready = true;
-        callback();
-      });
-    } else {
-      callback();
-    }
-  },
-  createJSONFile(test) {
-    context.newEntry().commit().then((entry) => {
-      const r = entry.getResource(true);
-      return r.putJSON({ a: 'v' }).then(() => {
-        entry.setRefreshNeeded(true);
-        return entry.refresh().then(() => {
-          test.ok(entry.getEntryInfo().getFormat() === 'application/json', 'Mimetype is not application/json as it should.');
-          return r.getJSON().then((data) => {
-            test.ok(_.isObject(data) && data.a === 'v', 'Json not set correctly.');
-            test.done();
-          });
-        });
-      });
-    }, () => {
+  setUp,
+  tearDown,
+  async createJSONFile(test) {
+    let entry;
+    try {
+      entry = await context.newEntry().commit();
+    } catch (err) {
       test.ok(false, 'Something went wrong when creating a File entry with JSON content.');
-    });
+    }
+
+    const r = entry.getResource(true);
+    await r.putJSON({ a: 'v' });
+    entry.setRefreshNeeded(true);
+    await entry.refresh();
+    test.ok(entry.getEntryInfo().getFormat() === 'application/json', 'Mimetype is not application/json as it should.');
+    const data = await r.getJSON();
+    test.ok(_.isObject(data) && data.a === 'v', 'Json not set correctly.');
+    test.done();
   },
-  createTextFile(test) {
-    context.newEntry().commit().then((entry) => {
-      const resource = entry.getResource(true);
-      return resource.putText('test').then(() => {
-        entry.setRefreshNeeded(true);
-        return entry.refresh().then(() => {
-          test.ok(entry.getEntryInfo().getFormat() === 'text/plain', 'Mimetype is not text/plain as it should.');
-          return resource.getText().then((data) => {
-            test.ok(_.isString(data) && data === 'test', 'Text not set correctly as resource.');
-            test.done();
-          });
-        });
-      });
-    }, () => {
+  async createTextFile(test) {
+    let entry;
+    try {
+      entry = await context.newEntry().commit();
+    } catch (err) {
       test.ok(false, 'Something went wrong when creating a Fileentry with text content.');
-    });
+    }
+    const resource = entry.getResource(true);
+    await resource.putText('test');
+    entry.setRefreshNeeded(true);
+    await entry.refresh();
+    test.ok(entry.getEntryInfo().getFormat() === 'text/plain', 'Mimetype is not text/plain as it should.');
+    const data = await resource.getText();
+    test.ok(_.isString(data) && data === 'test', 'Text not set correctly as resource.');
+    test.done();
   },
-  createXMLFile(test) {
-    context.newEntry().commit().then((entry) => {
-      const r = entry.getResource(true);
-      const DOMParser = utils.isBrowser() ? DOMParser : require('xmldom').DOMParser;
-      const parser = new DOMParser();
-
-      let xml = '<book></book>';
-      xml = parser.parseFromString(xml, 'text/xml');
-
-      return r.putXML(xml).then(() => {
-        entry.setRefreshNeeded(true);
-        return entry.refresh().then(() => {
-          test.ok(entry.getEntryInfo().getFormat() === 'text/xml', 'Mimetype is not text/plain as it should.');
-          return r.getXML().then((data) => {
-            if (utils.isBrowser()) {
-              test.ok(data instanceof Document && data.firstChild.nodeName === 'book',
-                'XML not stored correctly, document contains other xml than sent.');
-              test.done();
-            } else {
-              test.ok(_.isString(data) && data === '<book/>', 'XMl not set correctly as a resource.');
-              test.done();
-            }
-          });
-        });
-      });
-    }, () => {
+  async createXMLFile(test) {
+    let entry;
+    try {
+      entry = await context.newEntry().commit();
+    } catch (err) {
       test.ok(false, 'Something went wrong when creating a File entry with xml content.');
-    });
+    }
+    const r = entry.getResource(true);
+    const DOMParser = utils.isBrowser() ? DOMParser : require('xmldom').DOMParser;
+    const parser = new DOMParser();
+
+    let xml = '<book></book>';
+    xml = parser.parseFromString(xml, 'text/xml');
+
+    await r.putXML(xml);
+    entry.setRefreshNeeded(true);
+    await entry.refresh();
+    test.ok(entry.getEntryInfo().getFormat() === 'text/xml', 'Mimetype is not text/plain as it should.');
+    const data = await r.getXML();
+    if (utils.isBrowser()) {
+      test.ok(data instanceof Document && data.firstChild.nodeName === 'book',
+        'XML not stored correctly, document contains other xml than sent.');
+      test.done();
+    } else {
+      test.ok(_.isString(data) && data === '<book/>', 'XMl not set correctly as a resource.');
+      test.done();
+    }
+    finished = true;
   },
 };
