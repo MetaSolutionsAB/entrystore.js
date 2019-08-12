0a55ae67755af6a59860caf2affd6557154b822e
Valentino Hudhra <v.hudhra@gmail.com>
2019-08-12 16:02:44 +0200

    Update tests Cache.js

--- tests/Cache.js
+++ tests/Cache.js
@@ -1,8 +1,5 @@
-const config = require('./config');
 const store = require('../dist/EntryStore.node');
 
-const { repository } = config;
-new store.EntryStore(repository); // needed?
 const Cache = store.Cache;
 
 const e1 = {
@@ -16,32 +13,32 @@ const e1 = {
 
 exports.Cache = {
   initCache(test) {
-    const c = new Cache();
-    test.ok(c != null);
+    const cache = new Cache();
+    test.ok(cache != null);
     test.done();
   },
   basicCacheFunctionality(test) {
-    const c = new Cache();
-    c.cache(e1);
-    test.ok(c.get(e1.getURI()) === e1, 'Failed to retrieve cached entry.');
+    const cache = new Cache();
+    cache.cache(e1);
+    test.ok(cache.get(e1.getURI()) === e1, 'Failed to retrieve cached entry.');
     test.done();
   },
   invalidateCache(test) {
-    const c = new Cache();
-    c.cache(e1);
-    c.addCacheUpdateListener((event) => {
+    const cache = new Cache();
+    cache.cache(e1);
+    cache.addCacheUpdateListener((event) => {
       test.ok(event === 'allEntriesNeedRefresh');
       test.done();
     });
-    c.allNeedRefresh();
+    cache.allNeedRefresh();
   },
   listeners(test) {
-    const c = new Cache();
-    c.cache(e1);
-    c.addCacheUpdateListener((event) => {
+    const cache = new Cache();
+    cache.cache(e1);
+    cache.addCacheUpdateListener((event) => {
       test.ok(event === 'allEntriesNeedRefresh');
       test.done();
     });
-    c.allNeedRefresh();
+    cache.allNeedRefresh();
   },
 };
