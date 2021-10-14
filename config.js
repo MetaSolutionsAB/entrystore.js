f3ca6ec9aea9284916309c3e88a6ea8c1bde1e18
augustee <august@metasolutions.se>
2021-10-14 14:01:43 +0200

    Removed config files

--- config.js
+++
@@ -1,8 +0,0 @@
-window.config = {
-    repository: "http://dev.entryscape.com/store/",
-    username: "august@metasolutions.se",
-    password: "12341234",
-    contextId: "260",
-    entryId: "3"
-  };
-  
