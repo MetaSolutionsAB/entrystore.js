d16aa8135888d919fd5949273c416817c55c262a
augustee <august@metasolutions.se>
2021-10-27 12:14:10 +0200

    Removed unnecessary files

---
+++ tests/config.js
@@ -0,0 +1,9 @@
+export default {
+    repository: 'https://g.dev.entryscape.com/store/',
+    nonAdminUser: 'giorgos+pp3@metasolutions.se',
+    nonAdminPassword: 'stop&go2',
+    adminUser: 'admin',
+    adminPassword: 'adminadmin',
+    contextId: 1,
+    entryId: 1,
+};
