1e9c8b7ca82fd8c86a57178319e11b1e570837ab
augustee <august@metasolutions.se>
2021-10-28 16:50:25 +0200

    Removed config file

--- tests/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-  repository: 'https://a.dev.entryscape.com/store/',
-  nonAdminUser: 'august@metasolutions.se',
-  nonAdminPassword: 'August07',
-  adminUser: 'admin',
-  adminPassword: 'adminadmin',
-  contextId: 1,
-  entryId: 1,
-};
