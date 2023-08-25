71a4ea558bfc8a9740b86dc31fdec2f820b75c66
matthias <matthias@metasolutions.se>
2023-08-25 15:57:27 +0200

    Update to webpack 5

---
+++ webpack.browser.cjs
@@ -0,0 +1,49 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+//  target: 'node',
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'entrystore.js',
+    library: 'ESJS',
+  },
+  plugins: [
+    new webpack.DefinePlugin({ 'global.GENTLY': false }), // needed by superagent
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^\.\/locale$/,
+      contextRegExp: /moment/,
+    })
+  ],
+  context: __dirname, // string (absolute path!)
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [[
+              '@babel/preset-env', {
+                shippedProposals: true,
+                useBuiltIns: 'usage',
+                corejs: 3,
+                targets: {
+                  ie: 11,
+                },
+              },
+            ]],
+            plugins: [
+              '@babel/plugin-proposal-object-rest-spread',
+              '@babel/plugin-proposal-class-properties',
+              '@babel/plugin-syntax-dynamic-import',
+            ],
+          },
+        }],
+      },
+    ]
+  },
+};
---
+++ webpack.node.cjs
@@ -0,0 +1,51 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+//  target: 'node',
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'entrystore.node.cjs',
+//    library: 'EntryStore',
+    libraryTarget: 'commonjs2',
+  },
+  plugins: [
+    new webpack.DefinePlugin({ 'global.GENTLY': false }), // needed by superagent
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^\.\/locale$/,
+      contextRegExp: /moment/,
+    })
+  ],
+  context: __dirname, // string (absolute path!)
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [[
+              '@babel/preset-env', {
+                shippedProposals: true,
+                useBuiltIns: 'usage',
+                corejs: 3,
+                targets: {
+                  "node": "current"
+                }
+              },
+            ]],
+            plugins: [
+              '@babel/plugin-proposal-object-rest-spread',
+              '@babel/plugin-proposal-class-properties',
+              '@babel/plugin-syntax-dynamic-import',
+//              ['@babel/plugin-transform-modules-commonjs', { strictMode: false }],
+            ],
+          },
+        }],
+      },
+    ]
+  },
+};
